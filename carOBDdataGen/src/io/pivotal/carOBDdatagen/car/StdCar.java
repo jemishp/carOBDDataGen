35cd8c7f4e3bbc9e5723e7a05868ba7547493633
jpatel@pivotal.io <jpatel@pivotal.io>
2016-09-12 23:05:04 -0700

    adding a car

---
+++ carOBDdataGen/src/io/pivotal/carOBDdatagen/car/StdCar.java
@@ -0,0 +1,235 @@
+package io.pivotal.carOBDdatagen.car;
+
+public class StdCar {
+	
+	private int amountOfFuel;
+	private int tankCapacity;
+	private int currentMileage;
+    /* We use an int to track gears that a car is in
+     * 0 = Park
+     * 1 - 5 Different Gears 
+     * 10 = Reverse
+     */
+	private int currentGear;
+	private int currentSpeed;
+	private int currentRpm;
+	/*
+	 * 0 = no eBrake applied
+	 * 1 = eBrake is on
+	 */
+	private int eBrake;
+	private float engTemp;
+	private float insideTemp;
+	private float outsideTemp;
+	private float frontLeftPsi;
+	private float frontRightPsi;
+	private float rearLeftPsi;
+	private float rearRightPsi;
+	private String make;
+	private String model;
+	private int year;
+	
+	public StdCar (String make, String model , int year) {
+		this.make = make;
+		this.model = model;
+		this.year = year;
+	}
+
+	public int getAmountOfFuel() {
+		return amountOfFuel;
+	}
+
+	public void setAmountOfFuel(int amountOfFuel) {
+		this.amountOfFuel = amountOfFuel;
+	}
+
+	public int getTankCapacity() {
+		return tankCapacity;
+	}
+
+	public void setTankCapacity(int tankCapacity) {
+		this.tankCapacity = tankCapacity;
+	}
+
+	public int getCurrentMileage() {
+		return currentMileage;
+	}
+
+	public void setCurrentMileage(int currentMileage) {
+		this.currentMileage = currentMileage;
+	}
+
+	public int getCurrentGear() {
+		return currentGear;
+	}
+
+	public void setCurrentGear(int currentGear) {
+		this.currentGear = currentGear;
+	}
+
+	public int getCurrentSpeed() {
+		return currentSpeed;
+	}
+
+	public void setCurrentSpeed(int currentSpeed) {
+		this.currentSpeed = currentSpeed;
+	}
+
+	public int getCurrentRpm() {
+		return currentRpm;
+	}
+
+	public void setCurrentRpm(int currentRpm) {
+		this.currentRpm = currentRpm;
+	}
+
+	public int getEbrake() {
+		return eBrake;
+	}
+
+	public void setEbrake(int eBrake) {
+		this.eBrake = eBrake;
+	}
+
+	public float getEngTemp() {
+		return engTemp;
+	}
+
+	public void setEngTemp(float engTemp) {
+		this.engTemp = engTemp;
+	}
+
+	public float getInsideTemp() {
+		return insideTemp;
+	}
+
+	public void setInsideTemp(float insideTemp) {
+		this.insideTemp = insideTemp;
+	}
+
+	public float getOutsideTemp() {
+		return outsideTemp;
+	}
+
+	public void setOutsideTemp(float outsideTemp) {
+		this.outsideTemp = outsideTemp;
+	}
+
+	public float getFrontLeftPsi() {
+		return frontLeftPsi;
+	}
+
+	public void setFrontLeftPsi(float frontLeftPsi) {
+		this.frontLeftPsi = frontLeftPsi;
+	}
+
+	public float getFrontRightPsi() {
+		return frontRightPsi;
+	}
+
+	public void setFrontRightPsi(float frontRightPsi) {
+		this.frontRightPsi = frontRightPsi;
+	}
+
+	public float getRearLeftPsi() {
+		return rearLeftPsi;
+	}
+
+	public void setRearLeftPsi(float rearLeftPsi) {
+		this.rearLeftPsi = rearLeftPsi;
+	}
+
+	public float getRearRightPsi() {
+		return rearRightPsi;
+	}
+
+	public void setRearRightPsi(float rearRightPsi) {
+		this.rearRightPsi = rearRightPsi;
+	}
+
+	public String getMake() {
+		return make;
+	}
+
+	public void setMake(String make) {
+		this.make = make;
+	}
+
+	public String getModel() {
+		return model;
+	}
+
+	public void setModel(String model) {
+		this.model = model;
+	}
+
+	public int getYear() {
+		return year;
+	}
+
+	public void setYear(int year) {
+		this.year = year;
+	}
+
+
+	public void incrementFuelTank() {
+		if (this.amountOfFuel < 15 )
+			this.amountOfFuel++;
+	}
+
+	public void decrementFuelTank() {
+		if (this.amountOfFuel > 0)
+			this.amountOfFuel--;
+	}
+	
+	public void incrementCurrentMileage() {
+		if (this.currentMileage < 999999 )
+			this.currentMileage++;
+	}
+	
+	public void decrementCurrentMileage() {
+		if ( this.currentMileage > 25 )
+			this.currentMileage--;
+	}
+	
+	public void incrementCurrentGear() {
+		if ( this.currentGear == 0)
+			this.currentGear++;
+		if ( this.currentGear < 5 )
+			this.currentGear++;
+	}
+	
+	public void decrementCurrentGear() {
+		if ( this.currentGear > 0)
+			this.currentGear--;	
+	}
+	
+	public void putInReverse() {
+		this.currentGear = 10;
+	}
+	
+	public void getOutOfReverse() {
+		this.currentGear = 1;
+		
+	}
+
+	public void park() {
+		this.currentGear = 0;
+		this.eBrake = 1;
+	}
+	
+	public void accelerate() {
+		if (currentSpeed < 135  && currentRpm < 8 )
+			currentSpeed++;
+		    currentRpm++;			
+	}
+	
+	public void decelerate(){
+		if (currentSpeed > 0 && currentRpm > 1 )
+			currentSpeed--;
+			currentRpm--;
+	}
+	
+	
+	
+}
