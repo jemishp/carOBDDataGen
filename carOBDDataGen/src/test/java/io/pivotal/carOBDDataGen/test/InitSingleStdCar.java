12a0bf0aea8dc78be3b098422c7181ead5b96daf
jpatel@pivotal.io <jpatel@pivotal.io>
2016-09-13 16:10:47 -0700

    cleanup

--- carOBDDataGen/src/test/java/io/pivotal/carOBDDataGen/test/InitSingleStdCar.java
+++ carOBDDataGen/src/test/java/io/pivotal/carOBDDataGen/test/InitSingleStdCar.java
@@ -5,7 +5,6 @@ import org.apache.logging.log4j.Logger;
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 
 import io.pivotal.carOBDDatGen.car.StdCar;
