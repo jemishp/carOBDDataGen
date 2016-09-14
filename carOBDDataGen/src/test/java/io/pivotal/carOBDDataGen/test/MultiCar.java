3c7637b0fe6615d3becb16e7a27bf2683cab354c
jpatel@pivotal.io <jpatel@pivotal.io>
2016-09-14 16:12:01 -0700

    adding multicar tests

---
+++ carOBDDataGen/src/test/java/io/pivotal/carOBDDataGen/test/MultiCar.java
@@ -0,0 +1,43 @@
+package io.pivotal.carOBDDataGen.test;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.junit.Assert;
+import org.junit.Test;
+
+import io.pivotal.carOBDDatGen.car.StdCar;
+
+public class MultiCar {
+	static final Logger logger = LogManager.getLogger(InitSingleStdCar.class);
+	
+	@Test
+	public void testInitCars() throws Exception {
+		try {
+			List<StdCar> carList = new ArrayList<StdCar>() ;
+			Random rnd = new Random();
+			int a = rnd.nextInt(100);
+			for (int i=0; i<a; i++) {
+				StdCar car = new StdCar();
+				carList.add(car);
+				logger.debug((car.toString()));
+				Assert.assertNotNull(car.getMake());
+				Assert.assertNotNull(car.getModel());
+				Assert.assertNotNull(car.getYear());
+				Assert.assertNotNull(car.getVin());
+				Assert.assertNotNull(car.getTankCapacity());
+			}
+			Assert.assertEquals(a, carList.size());
+
+		} catch (Exception e) {
+			//fails
+			logger.debug("Exception Occured: " , e );
+			Assert.fail("Exception Occured: " +  e.toString());
+		}			
+		
+	}
+
+}
