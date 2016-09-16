291765620d7314e3e634cbcb34eb6f59627da360
jpatel@pivotal.io <jpatel@pivotal.io>
2016-09-16 16:42:33 -0700

    fixing neg rpm bug

--- carOBDDataGen/src/main/java/io/pivotal/carOBDDatGen/car/StdCar.java
+++ carOBDDataGen/src/main/java/io/pivotal/carOBDDatGen/car/StdCar.java
@@ -258,9 +258,12 @@ public class StdCar {
 	}
 	
 	public void decelerate(){
-		if (this.currentSpeed > 0 && this.currentRpm >= 1 ) {
+		if (this.currentSpeed > 0 && this.currentRpm > 1 ) {
 			this.currentSpeed--;
 			this.currentRpm--;
+			this.currentMileage++;
+		} else if ( this.currentSpeed > 0 && this.currentRpm == 1) {
+			this.currentSpeed--;
 			this.currentMileage++;
 		}
 	}
